d53be52d57c6067cf366b830cba5b1f2d9e5b291
David Achourbecove <davidac@edu.aac.ac.il>
2022-05-17 13:22:36 +0300

    Changes by afi23

--- src/CalcBmi.java
+++ src/CalcBmi.java
@@ -10,13 +10,10 @@ import java.awt.event.MouseEvent;
 
 public class CalcBmi  extends JFrame implements ActionListener {
 
-    private JPanel firstContentPanel;
+
     private JPanel secondContentPanel;
     private JPanel thirdContentPanel;
-    private JLabel firstName;
-    private JLabel lastName;
-    private JLabel age;
-    private JLabel gender;
+
     private JLabel bodyForm;
     private JLabel currentWright;
     private JLabel currentHeight;
@@ -24,11 +21,6 @@ public class CalcBmi  extends JFrame implements ActionListener {
 
     private JButton buttonSend;
     private JButton buttonCancel;
-    private JTextField textFieldFirstName;
-    private JTextField textFieldLastName;
-    private JTextField textFieldAge;
-    private JRadioButton manRadioButton;
-    private JRadioButton womanRadioButton;
     private JSlider slider1;
     private JRadioButton smallRadioButton;
     private JRadioButton mediumRadioButton;
@@ -37,65 +29,136 @@ public class CalcBmi  extends JFrame implements ActionListener {
     private JSlider slider2;
     private JLabel valueOfWeight;
     private  double slimness;
+    private UpperValues upperValues;
 
 
 
     public CalcBmi() {
         JFrame frame = new JFrame("CalcBmi");
          frame.setLayout(new  BorderLayout(10,10));
-        firstName = new JLabel("First Name:");
-        textFieldFirstName = new JTextField();
-        firstName.setBounds(20, 0, 200, 80);
-        textFieldFirstName.setBounds(90, 30, 70, 20);
-        firstContentPanel = new JPanel();
-        firstContentPanel.setLayout(null);
-        firstContentPanel.setBounds(10, 10, 580, 200);
-        firstContentPanel.add(firstName);
-        firstContentPanel.add(textFieldFirstName);
-        firstContentPanel.setBackground(Color.white);
-        frame.add(firstContentPanel);
+
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBackground(Color.GRAY);
 
+
+
         frame.setVisible(true);
         frame.setSize(Constants.WIDTH, Constants.HEIGHT);
+        upperValues=new UpperValues();
 
 
-        //-------------------------------------------------------------------------------
-        lastName = new JLabel("Last Name:");
-        textFieldLastName = new JTextField();
-        lastName.setBounds(20, -20, 200, 200);
-        textFieldLastName.setBounds(90, 70, 70, 20);
-        firstContentPanel.setBounds(0, 10, 58033, 600);
-        firstContentPanel.add(lastName);
-        firstContentPanel.add(textFieldLastName);
-        firstContentPanel.setPreferredSize(new Dimension(100,100));
-        JPanel jPanel=new JPanel();
-        jPanel.setPreferredSize(new Dimension(50,50));
+        frame.add(upperValues.getFirstContentPanel(),BorderLayout.NORTH);
 
-        frame.add(firstContentPanel,BorderLayout.EAST);
-        //-----------------------------------------------------------------------------------
-        age = new JLabel("Age:");
-        textFieldAge = new JTextField();
-        age.setBounds(20, 10, 200, 200);
-        textFieldAge.setBounds(55, 100, 70, 20);
-        firstContentPanel.add(age);
-        firstContentPanel.add(textFieldAge);
-        //---------------------------------------------------------------------------------}
-        gender=new JLabel("Gender:");
-        manRadioButton=new JRadioButton("Man");
-        womanRadioButton=new JRadioButton("Woman");
-        gender.setBounds(5,185,100,80);
-        manRadioButton.setBounds(60,200,100,50);
-        manRadioButton.setBackground(Color.white);
-        womanRadioButton.setBounds(160,200,100,50);
-        womanRadioButton.setBackground(Color.white);
-        firstContentPanel.add(gender);
-        firstContentPanel.add(manRadioButton);
-        firstContentPanel.add(womanRadioButton);
+
+
         //--------------------------------------------------------------------------------->
         secondContentPanel = new JPanel();
         bodyForm = new JLabel("Body Form:");
+        smallRadioButton = new JRadioButton("Small");
+        mediumRadioButton = new JRadioButton("Medium");
+        largeRadioButton = new JRadioButton("Large");
+        bodyForm.setBounds(0,200,10,80);
+        smallRadioButton.setBounds(60,220,100,50);
+        smallRadioButton.setBackground(Color.white);
+        mediumRadioButton.setBounds(60,220,100,50);
+        mediumRadioButton.setBackground(Color.white);
+        largeRadioButton.setBounds(60,220,100,50);
+        largeRadioButton.setBackground(Color.white);
+        secondContentPanel.add(bodyForm);
+        secondContentPanel.add(smallRadioButton);
+        secondContentPanel.add(mediumRadioButton);
+        secondContentPanel.add(largeRadioButton);
+        secondContentPanel.setBackground(Color.BLUE);
+        frame.add(secondContentPanel,BorderLayout.WEST);
+
+        //------------------------------------------------------------------------->
+        valueOfWeight = new JLabel("Actual Weight");
+        slider1 = new JSlider(40,180);
+       // slider1.add(valueOfWeight);
+        //valueOfWeight.setBounds(0,100,10,80);
+       // slider1.setBounds(5,100,10,80);
+        secondContentPanel.add(valueOfWeight);
+        secondContentPanel.add(slider1);
+        //-------------------------------------------------------------------------------
+        valueHeight = new JLabel("Height");
+        slider2 = new JSlider(40,140);
+        //valueHeight.setBounds(0,100,10,80);
+        secondContentPanel.add(valueHeight);
+        secondContentPanel.add(slider2);
+        //-------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        smallRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                slimness=Constants.SMALL;
+                mediumRadioButton.setSelected(false);
+                largeRadioButton.setSelected(false);
+
+            }
+        });
+        mediumRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                slimness=Constants.MEDIUM;
+                smallRadioButton.setSelected(false);
+                largeRadioButton.setSelected(false);
+
+            }
+        });
+        largeRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                slimness=Constants.LARGE;
+                smallRadioButton.setSelected(false);
+                mediumRadioButton.setSelected(false);
+            }
+        });
+
+        slider1.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                valueHeight.setText(slider1.getValue() + "cm");
+
+            }
+        });
+
+        slider2.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                valueOfWeight.setText(slider2.getValue() + "Kg");
+
+            }
+        });
+
+
 
 
 
@@ -103,7 +166,7 @@ public class CalcBmi  extends JFrame implements ActionListener {
 
 
     private void onOK() {
-        System.out.println(this.textFieldFirstName.getText() + " " + this.textFieldLastName.getText());
+
         this.setVisible(false);
        // ResultBmi ResultBmi= new ResultBmi(this.getName(), this.resultBmi(), this.getWeight(), this.resultOfIdealWeight());
     }
@@ -132,17 +195,17 @@ public class CalcBmi  extends JFrame implements ActionListener {
         return result;
     }
 
-    public double resultOfIdealWeight (){
-        double idealWeight=0;
-        double height=(double)this.slider1.getValue();
-        double age= Double.parseDouble(this.textFieldAge.getText());
-        idealWeight=(height-Constants.HUNDRED + (age/Constants.TEN)) *Constants.SMALL*this.slimness;
-        return idealWeight;
-    }
-
-    public String getName(){
-        return this.textFieldFirstName.getText() + " " + this.textFieldLastName.getText();
-    }
+//    public double resultOfIdealWeight (){
+//        double idealWeight=0;
+//        double height=(double)this.slider1.getValue();
+//        double age= Double.parseDouble(this.textFieldAge.getText());
+//        idealWeight=(height-Constants.HUNDRED + (age/Constants.TEN)) *Constants.SMALL*this.slimness;
+//        return idealWeight;
+//    }
+//
+//    public String getName(){
+//        return this.textFieldFirstName.getText() + " " + this.textFieldLastName.getText();
+//    }
 
     private double calculateBmi(){
         double height= (double) this.slider1.getValue()/100;
--- src/UpperValues.java
+++ src/UpperValues.java
@@ -1,12 +1,105 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-
-public class UpperValues {
+public class UpperValues  extends  JFrame implements ActionListener {
     private JPanel firstContentPanel;
     private JPanel secondContentPanel;
     private JPanel thirdContentPanel;
     private JLabel firstName;
     private JLabel lastName;
+    private JLabel age;
+    private JLabel gender;
+    private JTextField textFieldFirstName;
+    private JTextField textFieldLastName;
+    private JTextField textFieldAge;
+    private JRadioButton manRadioButton;
+    private JRadioButton womanRadioButton;
+
+    public UpperValues() {
+        firstName = new JLabel("First Name:");
+        textFieldFirstName = new JTextField();
+        firstName.setBounds(20, 0, 200, 80);
+        textFieldFirstName.setBounds(90, 30, 70, 20);
+        firstContentPanel = new JPanel();
+        firstContentPanel.setLayout(null);
+        firstContentPanel.setBounds(10, 10, 580, 200);
+        firstContentPanel.add(firstName);
+        firstContentPanel.add(textFieldFirstName);
+        firstContentPanel.setBackground(Color.white);
 
 
 
+        //-------------------------------------------------------------------------------
+        lastName = new JLabel("Last Name:");
+        textFieldLastName = new JTextField();
+        lastName.setBounds(170, -60, 200, 200);
+        textFieldLastName.setBounds(240, 30, 70, 20);
+        firstContentPanel.setBounds(0, 10, 58033, 600);
+        firstContentPanel.add(lastName);
+        firstContentPanel.add(textFieldLastName);
+        firstContentPanel.setPreferredSize(new Dimension(200,200));
+
+
+        //-----------------------------------------------------------------------------------
+        age = new JLabel("Age:");
+        textFieldAge = new JTextField();
+        age.setBounds(330, -60, 200, 200);
+        textFieldAge.setBounds(360, 30, 70, 20);
+        firstContentPanel.add(age);
+        firstContentPanel.add(textFieldAge);
+        //---------------------------------------------------------------------------------}
+        gender=new JLabel("Gender:");
+        manRadioButton=new JRadioButton("Man");
+        womanRadioButton=new JRadioButton("Woman");
+        gender.setBounds(35,40,100,80);
+        manRadioButton.setBounds(120,55,100,50);
+        manRadioButton.setBackground(Color.white);
+        womanRadioButton.setBounds(220,55,100,50);
+        womanRadioButton.setBackground(Color.white);
+        firstContentPanel.add(gender);
+        firstContentPanel.add(manRadioButton);
+        firstContentPanel.add(womanRadioButton);
+        //firstContentPanel.setPreferredSize(new Dimension(2));
+        //---------------------------------------------------------------------------
+
+        manRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                womanRadioButton.setSelected(false);
+            }
+        });
+        womanRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                manRadioButton.setSelected(false);
+            }
+        });
+
+        textFieldFirstName.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+
+
+            }
+        });
+    }
+
+    public JPanel getFirstContentPanel() {
+        return firstContentPanel;
+    }
+
+    public void setFirstContentPanel(JPanel firstContentPanel) {
+        this.firstContentPanel = firstContentPanel;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
